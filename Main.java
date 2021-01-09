b14520e162d20e6fdba67e3ccb665967001fa69a
Aastha Puri <63649181+aasthapuri@users.noreply.github.com>
2021-01-10 01:10:45 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,13 @@
 package com.company;
-
-
 import java.util.*;
-
 public class Main {
-
     public static void main(String[] args)
     {
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,1,1,2,3,4,3,4));
-
         HashMap<Integer,Integer> map = new HashMap<>();
         for (Integer i: list) {
             Integer i1=map.get(i);
             map.put(i,(i1==null)? 1: i1+1);
-
         }
         for (Map.Entry<Integer, Integer> map1 : map.entrySet()) {
             if (map1.getValue()==2){
