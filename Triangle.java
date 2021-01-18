b1383b51d5b204d894f6fd6d91367ee5be90e85c
Aastha Puri <aasthapuri28@gmail.com>
2021-01-18 23:09:43 +0530

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args)
     {
         for(int i=0;i<=5;i++){
-            for (int j=5;j>=i;j--){
+            for (int j=5;j>=i;j--)
                 System.out.print(" ");
-            }
+            
             for (int j=1;j<=i;j++){
                 System.out.print("* ");
             }
