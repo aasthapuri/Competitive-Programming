d2b600cb8c18d4a509c924f1ed278594009aad79
Aastha Puri <aasthapuri28@gmail.com>
2021-01-19 18:55:36 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
             }
             for (int j=1;j<=i;j++){
                 System.out.print("* ");
-            }
+            
             System.out.println();
         }
     }
